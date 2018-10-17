909d3a760a66e846aeabb494366bbd23136501c4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 19:30:40 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -51,6 +51,6 @@ mergedataframe
 statedf <- data.frame(state.name,state.center,state.area)
 statedf
 
-names(statedf)<-c("stateName","xstate","ystate","stateArea")
+colnames(statedf)<-c("stateName","xstate","ystate","stateArea")
 
 statedf
