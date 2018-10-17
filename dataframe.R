b8e4691ca70378de042fb2f7304d3c3c566514cd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 19:52:04 -0400

    Saving dataframe.R

--- dataframe.R
+++ dataframe.R
@@ -2,5 +2,10 @@
 library(ggplot2)
 library(ggmap)
 
-mergestatedata <- mergestatedf
-mergestatedata
+us <- map_data("state")
+
+datamap <- ggplot(mergestatedf, aes(map_id = stateName))
+datamap <- datamap + geom_map(map = us, aes(fill=StateArea))
+datamap <- datamap + expand_limits(x= us$long, y = us$lat)
+datamap <- datamap + coord_map() + ggtitle("State Area")
+datamap
