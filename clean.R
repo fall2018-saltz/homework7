552b1b39a9a8b62d0b6e8f5b30a5154b18cab5ef
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 17:58:20 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,3 +1,42 @@
 
-clean_data <- raw_data
+library(ggplot2)
+library(tibble)
+
+
+readStates <- function(states)
+{
+    #removing first row
+    
+    states <- states[-1,]
+    
+    #removing last row
+    
+    num.row <- nrow(states)
+    states <- states[-num.row,]
+    
+    #removing the first four columns
+    states <- states[,-1:-4]
+    
+    #changing names for remaining columns
+    colnames(states)<-c("stateName","population","popOver18","percentOver18")
+    
+    #returning the states dataset
+    return(states)
+    
+    
+    
+}
+
+cleanCensus <- readStates(raw_data)
+str(cleanCensus)
+
+#Loading the second dataset which is arrests.
+
+arrests <- USArrests
+arrests <- rownames_to_column(arrests, var = "stateName")
+arrests
+
+#Merging the data sets
+
+mergedataframe <- merge(cleanCensus,arrests, by = "stateName")
 mergedataframe
