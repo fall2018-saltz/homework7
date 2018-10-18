26231da6207c86cd1ecb7c9dc4c2fd74db5f7d75
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:58:29 -0400

    Saving dataframe.R

--- dataframe.R
+++ dataframe.R
@@ -21,8 +21,10 @@ datapoint
 
 #Since New York is the 32nd column, I checked the coordinates of New York. (I also cross checked online as
 # to confirm once again for New York City as I was confused between New York State and City.)
+
 mergestatedf[32,]
 
+#The coordinates of New York -75.1449,  43.1361)
 datazoom <- datapoint + scale_x_continuous(limits = c(-85.1449, -65.1449), expand = c(0,0)) + scale_y_continuous(limits=c(33.1361, 53.1361), expand = c(0,0))
 datazoom <- datazoom + coord_map() 
 datazoom
