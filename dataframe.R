b88c5d4d9c046260345883ea729f1d0c3612ddd2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:56:31 -0400

    Saving dataframe.R

--- dataframe.R
+++ dataframe.R
@@ -19,7 +19,8 @@ datamapmurder
 datapoint <- datamapmurder + geom_point(data = mergestatedf, aes(x=mergestatedf$xstate, y=mergestatedf$ystate , size = mergestatedf$population)) 
 datapoint
 
-#Since New York is the 32nd column, I checked the coordinates of New York. (I also cross checked online as)
+#Since New York is the 32nd column, I checked the coordinates of New York. (I also cross checked online as
+# to confirm once again for New York City as I was confused between New York State and City.)
 mergestatedf[32,]
 
 datazoom <- datapoint + scale_x_continuous(limits = c(-85.1449, -65.1449), expand = c(0,0)) + scale_y_continuous(limits=c(33.1361, 53.1361), expand = c(0,0))
