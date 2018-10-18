3a8f18a5e2adba65ae4820c4362f506781c492db
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 20:03:39 -0400

    Saving dataframe.R

--- dataframe.R
+++ dataframe.R
@@ -9,3 +9,9 @@ datamap <- datamap + geom_map(map = us, aes(fill=stateArea))
 datamap <- datamap + expand_limits(x= us$long, y = us$lat)
 datamap <- datamap + coord_map() + ggtitle("State Area")
 datamap
+
+datamapmurder <- ggplot(mergestatedf, aes(map_id = stateName))
+datamapmurder <- datamapmurder + geom_map(map = us, aes(fill=Murder))
+datamapmurder <- datamapmurder + expand_limits(x= us$long, y = us$lat)
+datamapmurder <- datamapmurder + coord_map() + ggtitle("Map by Murder")
+datamapmurder
